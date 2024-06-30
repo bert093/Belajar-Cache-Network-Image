960bbfc6f562a9b31667a0b3365448690ac2a1bc
bert093 <sas992203@gmail.com>
2024-06-30 17:15:11 +0800

    selesai, untuk sementara.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart'; // Tambahkan ini
 
 void main() {
   runApp(const MyApp());
@@ -25,8 +26,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/67405877_p0(lossless).png',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/67405877_p0(lossless).png',
                 ),
               ),
             ),
@@ -36,8 +37,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/119484129_p0(lossless).png',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/119484129_p0(lossless).png',
                 ),
               ),
             ),
@@ -47,8 +48,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/1161350729(lossless).jpg',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/1161350729(lossless).jpg',
                 ),
               ),
             ),
@@ -58,8 +59,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/112244258_p0(lossless).png',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/112244258_p0(lossless).png',
                 ),
               ),
             ),
@@ -69,8 +70,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/110607667(lossless).jpg',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/110607667(lossless).jpg',
                 ),
               ),
             ),
@@ -80,10 +81,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                // width: double.infinity,
-                // height: double.infinity,
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/115616593_p0(lossless).png',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/115616593_p0(lossless).png',
                   fit: BoxFit.cover, // menyesuaikan ukuran gambar dengan kontainer
                 ),
               ),
@@ -94,8 +93,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/115449074_p0(lossless).png',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/115449074_p0(lossless).png',
                 ),
               ),
             ),
@@ -105,24 +104,13 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.blue.withOpacity(0.2),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/119367220(lossless).jpg',
+                child: CachedNetworkImage( // Ganti Image.network dengan CachedNetworkImage
+                  imageUrl: 'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/119367220(lossless).jpg',
+
+                  // Dengan perubahan ini, gambar yang diambil dari jaringan akan disimpan dalam cache, sehingga tidak perlu diunduh ulang setiap kali aplikasi dijalankan
                 ),
               ),
             ),
-
-            // Align(
-            //   alignment: Alignment.centerLeft,
-            //   child: Padding(
-            //     padding: const EdgeInsets.all(8.0),
-            //     child: Container(
-            //       color: Colors.blue.withOpacity(0.2),
-            //       child: Image.network(
-            //         'https://raw.githubusercontent.com/bert093/flutter-image-assets/main/assets/images/119367220(lossless).jpg',
-            //       ),
-            //     ),
-            //   ),
-            // ),
           ],
         ),
       ),
